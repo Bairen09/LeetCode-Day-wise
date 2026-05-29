a9382d911da97b21947d6ca286965e58b44f5132
Bairan Chakraborty <159531175+Bairen09@users.noreply.github.com>
2026-05-29 23:32:16 +0530

    Time: 2 ms (91.69%), Space: 48 MB (6.4%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,6 +1,5 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        
         int j=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
@@ -8,10 +7,8 @@ class Solution {
                 j++;
             }
         }
-        while(j<nums.length){
-            nums[j]=0;
-            j++;
+        for(int i=j;i<nums.length;i++){
+            nums[i]=0;
         }
-
     }
 }
