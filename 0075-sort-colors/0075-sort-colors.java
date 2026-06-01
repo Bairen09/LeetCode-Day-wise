b66b2ce0629340ec0beca52ef18d7b81a3a71980
Bairan Chakraborty <159531175+Bairen09@users.noreply.github.com>
2026-06-01 14:16:36 +0530

    Time: 0 ms (100%), Space: 43.5 MB (40.78%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,24 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int left=0, right=nums.length-1,i=0;
+        while(i<=right){
+            if(nums[i]==2){
+                swap(i,right,nums);
+                right--;
+            }
+            else if(nums[i]==0){
+                swap(i,left,nums);
+                left++;
+                i++;
+            }
+            else{
+                i++;
+            }
+        }
+    }
+    public void swap(int a, int b,int nums[]){
+        int c=nums[a];
+        nums[a]=nums[b];
+        nums[b]=c;
     }
 }
