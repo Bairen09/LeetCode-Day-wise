f22f363ff197a89990472898b24d21ab0cfef587
Bairan Chakraborty <159531175+Bairen09@users.noreply.github.com>
2026-07-18 20:39:19 +0530

    Time: 16 ms (39.02%), Space: 47.6 MB (66.52%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,23 +1,25 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer>map=new HashMap<>();
-        for(int num:nums){
-            map.put(num, map.getOrDefault(num,0)+1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        PriorityQueue<Map.Entry<Integer,Integer>>pq= new PriorityQueue<>(
-            (a,b)->a.getValue()-b.getValue()
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a, b) -> map.get(a) - map.get(b)
         );
-        for(Map.Entry<Integer,Integer>entry:map.entrySet()){
-            pq.add(entry);
-            if(pq.size()>k){
-                pq.remove();
+
+        for (int num : map.keySet()) {
+            pq.add(num);
+            if (pq.size() > k) {
+                pq.poll();
             }
         }
-        int result[]=new int[k];
-        for(int i=k-1;i>=0;i--){
-            result[i]=pq.poll().getKey();
+
+        int[] result = new int[k];
+        for (int i = k - 1; i >= 0; i--) {
+            result[i] = pq.poll();
         }
         return result;
-
     }
 }
